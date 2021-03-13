ec2d018b30b7433a02b3b9a5dfeb844832e95186
ggzor <30713864+ggzor@users.noreply.github.com>
2021-03-13 15:44:47 -0600

    Fixing birds solution

--- Kattis/birds.hs
+++ Kattis/birds.hs
@@ -8,8 +8,8 @@ windows = foldr (zipWith (:)) (repeat []) . take 2 . tails
 
 main = do
   (l : d : n : xs) <- ints
-  let ls = concat $ [6] : map (\x -> [x - d, x + d]) (sort xs) ++ [[l - 6]]
-  print . foldl' (+) 0 . map (\[x, y] -> count d x y) $ windows ls
+  let ls = (6, 6) : map (\x -> (x - d, x + d)) (sort xs) ++ [(l - 6, l - 6)]
+  print . foldl' (+) 0 . map (\[(_, x), (y, _)] -> count d x y) $ windows ls
 
 count :: Int -> Int -> Int -> Int
 count d x y
